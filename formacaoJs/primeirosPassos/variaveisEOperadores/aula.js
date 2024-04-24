29b4e57e3ab3d5c8c39ffc8fc17d4741ec3308c0
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-04-24 15:46:53 -0300

    feat: class notes before the challenge

---
+++ formacaoJs/primeirosPassos/variaveisEOperadores/aula.js
@@ -0,0 +1,12 @@
+let variavel = 10 //pode ser mudado durante o codigo
+const pi = 3.14 //Não pode ser mais mudado
+
+let mais = 10 + 10
+let menos = 10 - 10
+let vezes = 10 * 10
+let divisao = 10 / 10
+
+console.log(mais)
+console.log(menos)
+console.log(vezes)
+console.log(divisao)
