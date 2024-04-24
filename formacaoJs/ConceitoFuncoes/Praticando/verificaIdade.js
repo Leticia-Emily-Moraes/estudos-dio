d4250b2ae12007ed4df09fa47fad8bc7c89560c8
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-04-24 19:23:54 -0300

    feat: Function that checks age of majority

---
+++ formacaoJs/ConceitoFuncoes/Praticando/verificaIdade.js
@@ -0,0 +1,10 @@
+function VerificaIdade(idade){
+    if(idade < 18){
+        console.log("Menor de idade")
+    }else{
+        console.log("Maior de idade")
+    }
+}
+
+VerificaIdade(13)
+VerificaIdade(31)
