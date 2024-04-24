c7b68f1ac2cbf0e054023f065c2c7d9ddb1143b3
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-04-24 17:47:31 -0300

    Exercise 2: BMI calculation

---
+++ formacaoJs/primeirosPassos/Praticando/CalculoImc.js
@@ -0,0 +1,17 @@
+const peso = 179;
+const Altura = 1.76;
+
+const IMC = peso / (Altura * Altura)
+console.log("IMC: " + IMC.toFixed(2))
+
+if(IMC < 18.5){
+    console.log("Abaixo do peso")
+}else if(IMC > 18.5 && IMC < 25){
+    console.log("Peso Normal")
+}else if(IMC > 25 && IMC < 30){
+    console.log("Acima do peso")
+}else if(IMC > 30 && IMC < 40){
+    console.log("Obeso")
+}else{
+    console.log("Obesidade Grave")
+}
