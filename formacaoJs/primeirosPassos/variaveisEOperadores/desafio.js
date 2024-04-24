743fa768c34f9158b8fbd0e47197e168656bde22
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-04-24 16:08:01 -0300

    fix: Limited decimal places with "toFixed(2)"

--- formacaoJs/primeirosPassos/variaveisEOperadores/desafio.js
+++ formacaoJs/primeirosPassos/variaveisEOperadores/desafio.js
@@ -4,4 +4,4 @@ const KmViajem = 60;
 
 const gastoViagem = (KmViajem / CombusPorKm) * PrecoCombus
 
-console.log("O gasto para uma viagem de "+ KmViajem + "km com um carro que faz "+ CombusPorKm + "km/l, com a gasolina a R$"+ PrecoCombus + " é de R$" + gastoViagem)
+console.log("O gasto para uma viagem de "+ KmViajem + "km com um carro que faz "+ CombusPorKm + "km/l, com a gasolina a R$"+ PrecoCombus + " é de R$" + gastoViagem.toFixed(2)) //toFixed() é para limitar as casas decimais
