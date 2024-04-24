16a2a4198b4fb490198742cd4baba0c35726abac
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-04-24 18:41:10 -0300

    fix: Factoring the code into functions

--- formacaoJs/primeirosPassos/Praticando/CalculoImc.js
+++ formacaoJs/primeirosPassos/Praticando/CalculoImc.js
@@ -1,17 +1,29 @@
-const peso = 179;
-const Altura = 1.76;
+(function main() {
+    const peso = 179;
+    const Altura = 1.76;
 
-const IMC = peso / Math.pow(Altura, 2) // função de potencia -> Math.pow(base, expoente) eleva a base ao expoente
-console.log("IMC: " + IMC.toFixed(2))
+    IMC = CalcImc(peso, Altura)
 
-if(IMC < 18.5){
-    console.log("Abaixo do peso")
-}else if(IMC > 18.5 && IMC < 25){
-    console.log("Peso Normal")
-}else if(IMC > 25 && IMC < 30){
-    console.log("Acima do peso")
-}else if(IMC > 30 && IMC < 40){
-    console.log("Obeso")
-}else{
-    console.log("Obesidade Grave")
+    console.log("IMC: " + IMC)
+    console.log("Classificação: " + ClassificaIMC(IMC))
+
+})() // função que invoca imediatamente
+
+function CalcImc(Peso, Altura) {
+    imc = Peso / Math.pow(Altura, 2) // função de potencia -> Math.pow(base, expoente) eleva a base ao expoente
+    return imc.toFixed(2)
+}
+
+function ClassificaIMC(imc) {
+    if (imc < 18.5) {
+        return "Abaixo do peso"
+    } else if (imc > 18.5 && imc < 25) {
+        return "Peso Normal"
+    } else if (imc > 25 && imc < 30) {
+        return "Acima do peso"
+    } else if (imc > 30 && imc < 40) {
+        return "Obeso"
+    } else {
+        return "Obesidade Grave"
+    }
 }
