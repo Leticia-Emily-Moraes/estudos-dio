a0558b542dc8866a7393e9add8a209f6160dd030
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-04-24 16:06:42 -0300

    feat: a fuel calculation was made on a trip

---
+++ formacaoJs/primeirosPassos/variaveisEOperadores/desafio.js
@@ -0,0 +1,7 @@
+const PrecoCombus = 5.27;
+const CombusPorKm = 16;
+const KmViajem = 60;
+
+const gastoViagem = (KmViajem / CombusPorKm) * PrecoCombus
+
+console.log("O gasto para uma viagem de "+ KmViajem + "km com um carro que faz "+ CombusPorKm + "km/l, com a gasolina a R$"+ PrecoCombus + " é de R$" + gastoViagem)
