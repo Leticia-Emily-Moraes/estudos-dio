510a8f2eec51b00963c8f02dc1b0bf19aa59ca46
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2025-01-20 12:00:00 -0300

    Tipos Primitivos de variaveis

--- FormacaoJavaFundamentals/demo/src/main/java/getstarted/tiposPrimitivos.java
+++ FormacaoJavaFundamentals/demo/src/main/java/getstarted/tiposPrimitivos.java
@@ -2,6 +2,17 @@ package getstarted;
 
 public class tiposPrimitivos {
     public static void main(String[] args) {
-        
+        // da menor memoria para a maior
+        byte n1 = 1;
+        short n2 = 1;
+        int n3 = 1;
+        long n4 = 1L;
+        float n5 = 1;
+        double n6 = 1;
+        //de caracteres
+        char c1 = 'v';
+        var s1 = "v";
+        // true e false
+        boolean bool = true;
     }
 }
