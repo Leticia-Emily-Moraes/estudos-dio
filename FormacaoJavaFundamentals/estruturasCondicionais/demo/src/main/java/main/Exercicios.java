9a093cc539c6ff360ce0f738e54823696dd523ed
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2025-01-31 12:00:00 -0300

    second solved exercise

--- FormacaoJavaFundamentals/estruturasCondicionais/demo/src/main/java/main/Exercicios.java
+++ FormacaoJavaFundamentals/estruturasCondicionais/demo/src/main/java/main/Exercicios.java
@@ -4,13 +4,37 @@ import java.util.Scanner;
 
 public class Exercicios {
     public static void main(String[] args) {
-        //Exercicio 1
+        // Exercicio 1
         var scan = new Scanner(System.in);
         System.out.println("Escolha um numero para a tabuada: ");
         var num = scan.nextInt();
         for (int i = 0; i <= 10; i++) {
-            
-            System.out.printf("%s X %s = %s\n",num, i, num * i );
+
+            System.out.printf("%s X %s = %s\n", num, i, num * i);
+        }
+
+        // exercicio 2
+        System.out.println("Insira seu peso: ");
+        var peso = scan.nextDouble();
+        System.out.println("Insira sua altura: ");
+        var altura = scan.nextDouble();
+        var imc = peso / (altura * altura);
+
+        System.out.printf("Seu IMC é: %.2f\n", imc);
+        System.out.printf("Situação: ");
+
+        if (imc < 18.5) {
+            System.out.println("Você está abaixo do peso.");
+        } else if (imc >= 18.5 && imc < 24.9) {
+            System.out.println("Peso normal.");
+        } else if (imc >= 25 && imc < 29.9) {
+            System.out.println("Levemente acima do peso.");
+        } else if (imc >= 30 && imc < 34.9) {
+            System.out.println("Obesidade grau I.");
+        } else if (imc >= 35 && imc < 39.9) {
+            System.out.println("Obesidade grau II.");
+        } else {
+            System.out.println("Obesidade grau III (mórbida).");
         }
     }
 }
